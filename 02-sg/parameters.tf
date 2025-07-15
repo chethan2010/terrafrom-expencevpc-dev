e3a6fcf1d0753e81ae3c31fd16cc1e8003f8a2d7
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-15 19:03:15 +0530

    expense-sg

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -2,6 +2,7 @@ resource "aws_ssm_parameter" "db_sg_id" {
   name  = "/${var.project_name}/${var.environment}/db_sg_id"
   type  = "String"
   value = module.db.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "backend_sg_id" {
@@ -16,32 +17,32 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   value = module.frontend.sg_id
 }
 
-resource "aws_ssm_parameter" "bastion_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
-  type  = "String"
-  value = module.bastion.sg_id
-}
+# resource "aws_ssm_parameter" "bastion_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
+#   type  = "String"
+#   value = module.bastion.sg_id
+# }
 
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
-  type  = "String"
-  value = module.vpn.sg_id
-}
+# resource "aws_ssm_parameter" "vpn_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
+#   type  = "String"
+#   value = module.vpn.sg_id
+# }
 
-resource "aws_ssm_parameter" "app_alb_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
-  type  = "String"
-  value = module.app_alb.sg_id
-}
+# resource "aws_ssm_parameter" "app_alb_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+#   type  = "String"
+#   value = module.app_alb.sg_id
+# }
 
-resource "aws_ssm_parameter" "web_alb_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/web_alb_sg_id"
-  type  = "String"
-  value = module.web_alb.sg_id
-}
+# resource "aws_ssm_parameter" "web_alb_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/web_alb_sg_id"
+#   type  = "String"
+#   value = module.web_alb.sg_id
+# }
 
-resource "aws_ssm_parameter" "ansible_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/ansible_sg_id"
-  type  = "String"
-  value = module.ansible.sg_id
-}
+# resource "aws_ssm_parameter" "ansible_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/ansible_sg_id"
+#   type  = "String"
+#   value = module.ansible.sg_id
+# }
--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend"s3"{
-    bucket="daws93-dev-bucket"
-    key="exp-sg-dev"
+    bucket="dev-daw9"
+     key= "expense-dev-sg/terraform.tfstate"
     region="us-east-1"
-    dynamodb_table="daws93-locking"   
+    dynamodb_table="daws83s"
 }
 }
 provider "aws" {
