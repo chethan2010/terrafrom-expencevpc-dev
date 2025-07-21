a48d3d7fb8bb528d1f376474533b5d3c163191fb
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-21 15:37:21 +0530

    expense-vpc

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -14,14 +14,14 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
 # join("," ,module.vpc.public_subnet_ids) 
 # #["id1","id2"] terraform format
 # # id1, id2 -> AWS SSM format
-# resource "aws_ssm_parameter" "private_subnet_ids" {
-#   name  = "/${var.Project_Name}/${var.environment}/private_subnet_ids"
-#   type  = "StringList"
-#   value = join(",",module.vpc.private_subnet_ids) # converting list to string list
-# }
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.Project_name}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",",module.vpc.private_subnet_cidrs) # converting list to string list
+}
 
-# resource "aws_ssm_parameter" "db_subnet_group_name" {
-#   name  = "/${var.Project_Name}/${var.environment}/db_subnet_group_name"
-#   type  = "String"
-#   value = module.vpc.database_subnet_group_name
-# }
+resource "aws_ssm_parameter" "db_subnet_group_name" {
+  name  = "/${var.Project_name}/${var.environment}/db_subnet_group_name"
+  type  = "String"
+  value = module.vpc.database_subnet_group_name
+} 
