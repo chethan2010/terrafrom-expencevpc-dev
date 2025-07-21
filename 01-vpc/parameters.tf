b37db4f6e3877ab1fea42a183b2bc0df44aa57ad
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-21 10:12:42 +0530

    expense-vpc

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -3,12 +3,12 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
-# resource "aws_ssm_parameter" "public_subnet_ids" {
-#   name  = "/${var.Project_Name}/${var.environment}/public_subnet_ids"
-#   type  = "StringList"
-#   value = join("," ,module.vpc.public_subnet_ids) # converting list to string list
-# }
-
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.Project_name}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = module.public_subnet_ids# converting list to string list
+} 
+# join("," ,module.vpc.public_subnet_ids) 
 # #["id1","id2"] terraform format
 # # id1, id2 -> AWS SSM format
 # resource "aws_ssm_parameter" "private_subnet_ids" {
