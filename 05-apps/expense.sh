d10542a97905ae0b405f0a935bc7652beb0810e9
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-21 19:15:52 +0530

    expense-ansible

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -2,7 +2,7 @@
 # user data will get sudo access
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/daws-78s/expense-ansible-roles.git
+git clone https://github.com/chethan2010/expense-roles-ansible.git
 cd expense-ansible-roles
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
--- 05-apps/provider.tf
+++ 05-apps/provider.tf
@@ -1,19 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-       version = "5.64.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.92.0"
     }
   }
-  backend "s3" {
-    bucket="daws93-dev-bucket"
-    key="exp-app-dev"
-    region="us-east-1"
-    dynamodb_table="daws93-locking" 
-  }
-}
 
-#provide authentication here
-provider "aws" {
-  region = "us-east-1"
+  backend "s3" {
+    bucket         = "dev-daw9"
+    key            = "expense-dev-ansible/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "daws83s"
+  }
 }
